264293a9f638b3a7e86792a0ac28ce7f0693cb0c
vishalSignh123 <116246501+vishalSignh123@users.noreply.github.com>
2022-11-01 15:05:13 +0530

    Delete env.ts

--- src/env.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-
-config();
-
-export const MONGO_DB_CONNECTION_STRING = process.env.MONGO_DB_CONNECTION_STRING || 'mongodb://localhost:27017/architecture';
-export const JWT_SECRET = process.env.JWT_SECRET || "thiisecret865776rr4e*&&*e"
-export const PORT = process.env.PORT || 4002
